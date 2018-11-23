b06df0acdd58926794e8606698019634295af926
Rune Warhuus <rune@dinkdonk.no>
2018-11-23 17:51:24 +0100

    Finished for real

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd "${BASH_SOURCE%/*}/../" || exit
+
+# DOMAIN=$(node -e "console.log(require('./package.json').domain);")
+
+# echo $DOMAIN > site/CNAME
+# echo 'google-site-verification: googleab6fe5ea20df429c.html' > build/googleab6fe5ea20df429c.html
+
+git checkout -b gh-pages
+cp node_modules/jsqr/dist/jsQR.js site/jsQR.js
+git add -f site
+git commit -m 'Deploy to gh-pages'
+git push origin `git subtree split --prefix build`:gh-pages --force
+git checkout master
+git branch -D gh-pages
