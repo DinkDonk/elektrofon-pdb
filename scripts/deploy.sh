9ea238af73ecede21ea58f6a1a46ed32448ebfea
Rune Warhuus <rune@dinkdonk.no>
2018-11-23 17:52:55 +0100

    Stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,6 @@ git checkout -b gh-pages
 cp node_modules/jsqr/dist/jsQR.js site/jsQR.js
 git add -f site
 git commit -m 'Deploy to gh-pages'
-git push origin `git subtree split --prefix build`:gh-pages --force
+git push origin `git subtree split --prefix site`:gh-pages --force
 git checkout master
 git branch -D gh-pages
